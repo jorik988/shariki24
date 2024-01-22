5ee560e8a6e6943fa8f05d85680faf3dbc68f109
jorik988 <vs358@yandex.ru>
2024-01-22 22:07:50 +0700

    search final

--- goods/utils.py
+++ goods/utils.py
@@ -1,20 +1,37 @@
-from django.db.models import Q #for search
+from django.db.models import Q  # for search
 from goods.models import Products
-from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector, SearchHeadline
+
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
-    
+
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
-    
-    
-    
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel='</span>',
+        )
+    )
 
-
-
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    return result
 
     # keywords = [word for word in query.split() if len(word) > 2]   #создаем список слов из запроса если длина более 2
     # q_objects = Q() #создаем переменную для Q объектов
