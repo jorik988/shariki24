1e0b029012570eb272f59e5c2b504b3a6ab142ee
jorik988 <vs358@yandex.ru>
2024-02-02 12:21:14 +0700

    cart2 templates ok

--- users/views.py
+++ users/views.py
@@ -73,3 +73,9 @@ def logout(request):
     messages.success(request, f"{request.user.username}, вы вышли из акаунта.")
     auth.logout(request)
     return redirect(reverse('main:index'))
+
+def users_cart(request):
+    
+    return render(request, 'users/users_cart.html')
+
+
