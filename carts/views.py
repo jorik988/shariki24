1d13727b6871a98a7678bdd45ef42a7f54c8c448
jorik988 <vs358@yandex.ru>
2024-02-05 13:57:02 +0700

    cart4 (cart remove)

--- carts/views.py
+++ carts/views.py
@@ -24,5 +24,8 @@ def cart_add(request, product_slug):
 
 def cart_change(request, product_slug):
     ...
-def cart_remove(request, product_slug):
-    ...
+def cart_remove(request, cart_id):
+    # cart = Cart.objects.get(id=cart_id)
+    # cart.delete()
+    Cart.objects.filter(id=cart_id).delete()
+    return redirect(request.META['HTTP_REFERER'])
