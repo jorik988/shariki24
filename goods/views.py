157abc82cccb24fe2602ffe1afd79a0d7c906020
Vitaliy Zhuravlev <vs358@yandex.ru>
2024-04-15 00:32:15 +0700

    test fix

--- goods/views.py
+++ goods/views.py
@@ -41,7 +41,7 @@ def catalog(request, category_slug=None):
 def product(request, product_slug):
     
     product = Products.objects.get(slug=product_slug)
-    base_products = product.base_products.all()
+    base_products = product.base_products.all().order_by('id')
     total_base_product = base_products.count()
     context = {
         "title": "мойшарик.рф - Товар",
